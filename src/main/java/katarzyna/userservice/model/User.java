b19e154b3e97411b2b0b124f9e5c4bcbde7b8cd3
Katarzyna <katarzyna.niziol1993@gmail.com>
2022-09-04 21:12:30 +0200

    Initial commit

---
+++ src/main/java/katarzyna/userservice/model/User.java
@@ -0,0 +1,49 @@
+package katarzyna.userservice.model;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+    private String lastaName;
+
+    public User() {}
+
+    public User(String email, String password, String name, String lastaName) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.lastaName = lastaName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastaName() {
+        return lastaName;
+    }
+
+    public void setLastaName(String lastaName) {
+        this.lastaName = lastaName;
+    }
+}
